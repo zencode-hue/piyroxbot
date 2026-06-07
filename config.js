3e9c059631bb1d74df52e0bdc9e651b5ffec9160
Preshcode2 <omololaaso22@gmail.com>
2026-06-07 15:57:34 +0100

    fixing11

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   token: process.env.BOT_TOKEN,
   prefix: ".",
-  guildId: "854838937667665930",
-  staffRoleId: "1051221648110789694",
-  ticketCategoryId: "1313753305444257813", // Replace with actual category ID
+  guildId: "1256877681719377940",
+  staffRoleId: "1256882378794729572",
+  ticketCategoryId: "1313753305444257813", // This needs to be a valid Discord Category ID
 };
