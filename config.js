1501ab05ac17376cbe454ae5618ad23454fb709e
Preshcode2 <omololaaso22@gmail.com>
2026-06-07 15:49:47 +0100

    fix

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   prefix: ".",
   guildId: "854838937667665930",
   staffRoleId: "1051221648110789694",
-  ticketCategoryId: "1234567890", // Replace with actual category ID
+  ticketCategoryId: "1313753305444257813", // Replace with actual category ID
 };
